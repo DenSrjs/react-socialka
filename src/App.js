5be4f6e8fba4d1fd227d6df870501550a241b9b9
Alex <denalexsr@yandex.ru>
2022-05-11 01:03:40 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React from 'react';
 import Header from "./components/Header/Header";
 import NavBar from "./components/NavBar/NavBar";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+import {Route} from "react-router-dom"
 import './App.css';
+import {Routes} from "react-router";
 
-const App =()=> {
+const App =(props)=> {
   return (
     <div className='app-wrapper'>
       <Header />
       <NavBar />
-      <Profile />
+      <div className='app-wrapper__content'>
+          <Routes>
+              <Route path="/profile" element={<Profile/>}/>
+              <Route path="/dialogs" element={<Dialogs/>}/>
+          </Routes>
+      </div>
     </div>
   );
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = ()=>{
     return(
-        <div className={s.content}>
+        <div>
             <div>
                 <img src="" alt="фон"/>
             </div>
