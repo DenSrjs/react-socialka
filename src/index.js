2d075f89eef6d4683fdb0a0362c67528cdfcfa1a
Alex <denalexsr@yandex.ru>
2022-06-18 17:32:33 +0300

    added method dispatch(action)

--- src/index.js
+++ src/index.js
@@ -10,14 +10,14 @@ let rerenderEntireTree = (state)=>{
         <React.StrictMode>
             <BrowserRouter>
                 <App state = {state}
-                     addPost={store.addPost.bind(store)}
-                     updateNewPostText={store.updateNewPostText.bind(store)}/>
+                     dispatch={store.dispatch.bind(store)}/>
             </BrowserRouter>
         </React.StrictMode>
     );
 }
 
 rerenderEntireTree(store.getState())
+
 store.subscribe(rerenderEntireTree)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -2,8 +2,8 @@ let store = {
     _state: {
         profilePage: {
             posts: [
-                {id: 1, message: "hello", likeCounts: 45 },
-                {id: 2, message: "hi",  likeCounts: 12},
+                {id: 1, message: "hello", likeCounts: 45},
+                {id: 2, message: "hi", likeCounts: 12},
             ],
             newPostText: '',
         },
@@ -21,30 +21,33 @@ let store = {
         },
         sidebar: {},
     },
-    getState(){
-        return this._state
-    },
-    _callSubscriber (){
+    _callSubscriber() {
         console.log('state changed')
     },
-    addPost (){
-        let newPost = {
-            id: 5,
-            message: this._state.profilePage.newPostText,
-            likesCount: 0,
-        };
 
-        this._state.profilePage.posts.push(newPost);
-        this._state.profilePage.newPostText = '';
-        this._callSubscriber(this._state);
+    getState() {
+        return this._state
     },
-    updateNewPostText (newText) {
-        this._state.profilePage.newPostText = newText;
-        this._callSubscriber(this._state);
-    },
-    subscribe (observer){
+    subscribe(observer) {
         this._callSubscriber = observer
     },
+
+    dispatch(action) {
+        if (action.type === 'ADD-POST') {
+            let newPost = {
+                id: 5,
+                message: this._state.profilePage.newPostText,
+                likesCount: 0,
+            };
+
+            this._state.profilePage.posts.push(newPost);
+            this._state.profilePage.newPostText = '';
+            this._callSubscriber(this._state);
+        }else if(action.type === 'UPDATE-NEW-POST-TEXT'){
+            this._state.profilePage.newPostText = action.newText;
+            this._callSubscriber(this._state);
+        }
+    }
 }
 
 export default store;
